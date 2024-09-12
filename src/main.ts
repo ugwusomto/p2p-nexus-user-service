7990c67ecc97e6417ee2c7f28e07688d090f592c
ugwu somto <ugwujude42@gmail.com>
2024-09-12 09:54:15 +0100

    set the user service to a microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: "localhost",
+      port: 6379,
+      retryAttempts: 5,
+      retryDelay: 1000,
+    }
+  });
+  await app.listen();
 }
 bootstrap();
